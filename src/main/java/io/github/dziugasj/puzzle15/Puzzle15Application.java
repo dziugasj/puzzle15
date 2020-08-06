55b1827d4c920271cc850b5ff75a226c87ba0bbc
Džiugas Juozapaitis <dziugasj@gmail.com>
2020-08-06 14:06:53 +0300

    Generated with Spring initializr

---
+++ src/main/java/io/github/dziugasj/puzzle15/Puzzle15Application.java
@@ -0,0 +1,13 @@
+package io.github.dziugasj.puzzle15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Puzzle15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Puzzle15Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dziugasj/puzzle15/Puzzle15ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dziugasj.puzzle15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Puzzle15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
