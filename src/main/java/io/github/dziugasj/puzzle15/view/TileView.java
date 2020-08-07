42ee3d95e96f93218fbfefa51c29500d68db58dd
Džiugas Juozapaitis <dziugasj@gmail.com>
2020-08-07 20:30:18 +0300

    TileView made immutable

--- src/main/java/io/github/dziugasj/puzzle15/view/TileView.java
+++ src/main/java/io/github/dziugasj/puzzle15/view/TileView.java
@@ -2,11 +2,17 @@ package io.github.dziugasj.puzzle15.view;
 
 import java.util.Map;
 
+import static com.google.common.collect.ImmutableMap.copyOf;
+
+
+/**
+ * An immutable view of the Tile
+ */
 public class TileView {
     private final Map<Integer, String> tiles;
 
     public TileView(Map<Integer, String> tiles) {
-        this.tiles = tiles;
+        this.tiles = copyOf(tiles);
     }
 
     public Map<Integer, String> getTiles() {
