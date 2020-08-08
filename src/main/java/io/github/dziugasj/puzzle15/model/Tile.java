291664041120ea3c1d325b865c20879a974ad2d8
Džiugas Juozapaitis <dziugasj@gmail.com>
2020-08-08 15:36:09 +0300

    lombok added

--- src/main/java/io/github/dziugasj/puzzle15/model/Tile.java
+++ src/main/java/io/github/dziugasj/puzzle15/model/Tile.java
@@ -1,12 +1,13 @@
 package io.github.dziugasj.puzzle15.model;
 
-import com.google.common.base.Objects;
+import lombok.EqualsAndHashCode;
 
 import java.util.Optional;
 
 /**
  * An immutable object representing Tile state.
  */
+@EqualsAndHashCode
 class Tile {
     private final Optional<Integer> value;
 
@@ -24,25 +25,5 @@ class Tile {
 
     public String getView() {
         return value.map(Object::toString).orElse("");
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tile tile = (Tile) o;
-        return Objects.equal(value, tile.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(value);
-    }
-
-    @Override
-    public String toString() {
-        return "Tile{" +
-                "value=" + value +
-                '}';
     }
 }
