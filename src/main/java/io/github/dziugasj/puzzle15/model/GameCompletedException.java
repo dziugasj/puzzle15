29b3a3db96698bb7e56f8237b00a34eb10e4c7ed
Džiugas Juozapaitis <dziugas.juozapaitis@addendum.lt>
2020-08-10 06:39:27 +0300

    Compiler warnings fixed

--- src/main/java/io/github/dziugasj/puzzle15/model/GameCompletedException.java
+++ src/main/java/io/github/dziugasj/puzzle15/model/GameCompletedException.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 public class GameCompletedException extends RuntimeException {
+    static final long serialVersionUID = -3387516993174229948L;
+
     public GameCompletedException(String gameId) {
         super("Game was completed with gameId: " + gameId);
     }
--- src/main/java/io/github/dziugasj/puzzle15/model/MoveNotPossibleException.java
+++ src/main/java/io/github/dziugasj/puzzle15/model/MoveNotPossibleException.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 public class MoveNotPossibleException extends RuntimeException{
+    static final long serialVersionUID = -3387516993174229999L;
+
     public MoveNotPossibleException(int position) {
         super("Cannot move tile in position: " + position);
     }
